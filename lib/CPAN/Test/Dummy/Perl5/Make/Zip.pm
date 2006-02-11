18782bbe1272214da0633d6b4f458e2c9dbabe28
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2006-02-11 11:30:58 +0000

    Import of ANDK/CPAN-Test-Dummy-Perl5-Make-Zip-1.01 from CPAN.
    gitpan-cpan-distribution: CPAN-Test-Dummy-Perl5-Make-Zip
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         ANDK/CPAN-Test-Dummy-Perl5-Make-Zip-1.01.zip
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

---
+++ lib/CPAN/Test/Dummy/Perl5/Make/Zip.pm
@@ -0,0 +1,37 @@
+package CPAN::Test::Dummy::Perl5::Make::Zip;
+
+$VERSION = '1.01';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+CPAN::Test::Dummy::Perl5::Make::Zip - CPAN Test Dummy
+
+=head1 SYNOPSIS
+
+    use CPAN::Test::Dummy::Perl5::Make::Zip;
+
+=head1 DESCRIPTION
+
+This module is part of CPAN.pm with the single purpose of testing
+CPAN.pm itself.
+
+Contains no functionality, and will never do so.
+
+=head1 AUTHOR
+
+Andreas Koenig, based PITA by Adam Kennedy.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Andreas Koenig
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
